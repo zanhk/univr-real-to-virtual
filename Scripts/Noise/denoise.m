319b42b903390b018fb07d90e56c077c2b4bd8e6
Giulio Zanchetta <zank@outlook.it>
2020-03-12 09:33:24 +0100

    chore: add various models and scripts from unity projects

--- Scripts/Noise/denoise.m
+++
@@ -1,9 +0,0 @@
-RGB = imread('noise.jpg');
-I = rgb2gray(RGB);
-K = wiener2(I,[5 5]);
-figure('units','normalized','outerposition',[0 0 1 1])
-subplot(1,2,1),imshow(I);
-title('Noised Image');
-subplot(1,2,2),imshow(K);
-title('Denoised Image');
-imwrite(K, 'denoised.jpg','jpg');
